1949b32d4f1b3574f981d8ff8640b07e50d97d48
Alvin Monsi Mammoottil <S290871@students.cdu.edu.au>
2020-08-19 11:03:20 +0930

    Refactored the code and added blank spaces

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,7 +1,22 @@
 from english_words import english_words_lower_set
 import random
 
-word_list = list(english_words_lower_set)
-word = random.choice(word_list)
-word = word.lower()
-print("\n"+ word +"\n")
+def generate_random_word():
+    word_list = list(english_words_lower_set)
+    word = random.choice(word_list)
+    word = word.lower()
+    return word
+
+generated_word = generate_random_word()
+print("\n"+ generated_word + "\n")
+
+display=[]
+display.extend(generated_word)
+
+for i in range(len(display)):
+    display[i]= "_"
+print(" ".join(display))
+
+print(len(generated_word))
+
+print(len(display))
