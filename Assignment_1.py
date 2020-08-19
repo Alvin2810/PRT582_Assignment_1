6c923e9a81216c79031d1242db66891a76e9926e
Alvin Monsi Mammoottil <S290871@students.cdu.edu.au>
2020-08-19 10:15:49 +0930

    Added code to generate a random word

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,4 +1,7 @@
 from english_words import english_words_lower_set
 import random
 
-print("\n\nWELCOME AND LETS PLAY HANGMAN\n\nHangman is a word game where the goal is  to find the word.\nAs a player you will be able to guess a letter and if the\nchosen letter exists in the answer, then all places in the\nanswer where that letter appear will be revealed.\n\nEvery time you guess a wrong letter 1 life will be deducted\nfrom your life. You will start the game with 10 lives once\nyou reach 0 lives and still fail to find the word the game\nwill be over.\n\n\nGood luck and let the game begin.\n")
+word_list = list(english_words_lower_set)
+word = random.choice(word_list)
+word = word.lower()
+print("\n"+ word +"\n")
