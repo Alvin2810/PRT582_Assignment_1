ce42e7e32ecc31db5c643ed2a3b93d26cec06657
Alvin Monsi Mammoottil <S290871@students.cdu.edu.au>
2020-08-28 19:36:51 +0930

    Refactoring 4

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,16 +1,19 @@
 from english_words import english_words_lower_set
 import random
 # declaring variables
-lives = 5
+lives_left = 5
 no_changes_made=[]
-used=[]
+used_words_list=[]
 blank_spaces=[]
+
+#defining the function to create a random word
 def generate_random_word():
     word_list = list(english_words_lower_set)
     word = random.choice(word_list)
     word = word.lower()
     return word
 
+#defining the function to check if the guess is a letter or not
 def isnotaletter(guess):
     if guess.isalpha() == False:
         return True
@@ -19,9 +22,10 @@ def isnotaletter(guess):
     else:
         return False
 
-generated_word = generate_random_word()
+generated_word = generate_random_word() #creating a random word
 print("\n"+ generated_word + "\n")
 
+#creating blank spaces for each letter in the word
 for letters in generated_word:
     blank_spaces.append("_")
 print(" ".join(blank_spaces))
@@ -32,29 +36,38 @@ print("Number of blank space: "+ str(len(blank_spaces))+"\n")
 no_changes_made.extend(blank_spaces)
 
 while "_" in blank_spaces:
-    print("\nLives: "+str(lives)) 
-    guess = input("\nGuess a letter: ").lower()
-    used.append(guess)
-    for number in range(len(generated_word)):
+    print("\nLives left: "+str(lives_left)) 
+
+    guess = input("\nGuess a letter: ")
+    guess.lower()
+
+    used_words_list.append(guess) #adding the newly guessed character into the used words list
+
+    #checking if the guess is in the word
+    for number in range(len(generated_word)): 
         if generated_word[number] == guess:
             blank_spaces[number] = guess
+    
+    #if no changes are made then the guess was not correct and 1 life is deducted
     if no_changes_made == blank_spaces:
         print("\nSorry you guessed wrong")
-        lives = lives-1  
-     
+        lives_left = lives_left-1  
+    
+    #checking if the guess is a letter or not
     if isnotaletter(guess):
-        lives+=1
+        lives_left+=1
         print("\nPlease make sure that you are entering a single valid letter rather than symbols, numbers or a string of characters")
     
-    if lives < 0:
+    #checking if the player has ran out of lives 
+    if lives_left < 0:
         print("\nYou have run out of lives\n\nThe word was:  "+ generated_word+"\n")
         break 
-
+    
     for number in range(len(blank_spaces)):
         no_changes_made[number] = blank_spaces[number]
         
     print("\n"+" ".join(blank_spaces))
-    print("\nused words: "+ str(used)+"\n")
+    print("\nused words: "+ str(used_words_list)+"\n")
 
 if "_" not in blank_spaces:
     print("\nCongratulations you have found the word\n")
